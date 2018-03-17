2672e3501feab5bda512703a79f0d2b7cac69a3b
Daniele Monteleone <daniele@monteleone.ml>
2018-03-17 22:04:23 +0100

    Add a fetch wrapper which sends auth token

--- src/api/auth/service.ts
+++ src/api/auth/service.ts
@@ -5,6 +5,16 @@ const AUTH_TOKEN_KEY = "aat"
 export const getAuthURL = () =>
   `${loginURL}?redirect_uri=${escape(`${location.origin}/callback`)}&response_type=token`
 
+export const login = () => location.replace(getAuthURL())
+
 export const setToken = (token: string) => localStorage.setItem(AUTH_TOKEN_KEY, token)
 
 export const getToken = () => localStorage.getItem(AUTH_TOKEN_KEY)
+
+export const callAPI = (input: RequestInfo, init?: RequestInit) => fetch(input, {
+  ...init,
+  headers: {
+    ...(init != null ? init.headers : null),
+    "Authorization": `Bearer ${getToken() || login()}`
+  }
+})
--- src/api/event.ts
+++ src/api/event.ts
@@ -1,8 +1,9 @@
-import { eventsCountURL, eventBinsURL } from "./url"
+import { callAPI } from "./auth/service"
 import { Bin } from "./type/event"
+import { eventBinsURL, eventsCountURL } from "./url"
 
 export async function getEventsCount(): Promise<number> {
-  const response = await fetch(eventsCountURL)
+  const response = await callAPI(eventsCountURL)
   const text = await response.text()
   return Number(text)
 }
@@ -11,7 +12,7 @@ export async function getEventBins(sample: string, start: Date, stop: Date) {
   const headers = new Headers()
   headers.append("Content-Type", "application/json")
 
-  const response = await fetch(eventBinsURL, {
+  const response = await callAPI(eventBinsURL, {
     method: "POST",
     headers,
     body: JSON.stringify({ sample, start, stop })
