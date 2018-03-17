c97d646b40f2180f8e03f8c0ae20629feb6a3438
Daniele Monteleone <daniele@monteleone.ml>
2018-03-17 19:37:37 +0100

    Add methods to api wrapper

--- src/api/type/event.ts
+++ src/api/type/event.ts
@@ -1,42 +1,74 @@
 import { parseArray } from "./array"
 
+export class Event {
+  static parse(json: string): Event {
+    return JSON.parse(json, Event.reviver)
+  }
+
+  static reviver(key: string, value: {}): {} {
+    switch (key) {
+      case "":
+        return value
+      case "id":
+        if (typeof value !== "number")
+          throw new SyntaxError('"id" must be a number')
+        return value
+      case "time":
+        if (typeof value !== "string")
+          throw new SyntaxError('"time" must be a string')
+        const time = Date.parse(value)
+        if (isNaN(time))
+          throw new SyntaxError(`"${value}" could not be parsed as date`)
+        return new Date(time)
+      case "press":
+        if (typeof value !== "number")
+          throw new SyntaxError('"press" must be a number')
+        return value
+      default:
+        throw new SyntaxError(`unexpected key: "${key}"`)
+    }
+  }
+
+  constructor(public id: number, public time: Date, public press: number) { }
+}
+
 export class Bin {
-    static parse(json: string): Bin {
-        return JSON.parse(json, Bin.reviver)
+  static parse(json: string): Bin {
+    return JSON.parse(json, Bin.reviver)
+  }
+
+  static parseMany(json: string): Bin[] {
+    if (json === "") { return [] }
+    const array = parseArray<Bin>(json)
+    const elements = [] as Bin[]
+
+    for (const element of array) {
+      elements.push(Bin.parse(JSON.stringify(element)))
     }
 
-    static parseMany(json: string): Bin[] {
-        if (json === "") { return [] }
-        const array = parseArray<Bin>(json)
-        const elements = [] as Bin[]
-        
-        for (const element of array) {
-            elements.push(Bin.parse(JSON.stringify(element)))
+    return elements
+  }
+
+  private static reviver(key: string, value: {}): {} {
+    switch (key) {
+      case "":
+        return value
+      case "start":
+        if (typeof value !== "string")
+          throw new SyntaxError('"start" must be a string')
+        const start = Date.parse(value)
+        if (isNaN(start))
+          throw new SyntaxError(`"${value}" could not be parsed as date`)
+        return new Date(start)
+      case "count":
+        if (typeof value !== "number") {
+          throw new SyntaxError('"count" must be a number')
         }
-
-        return elements
+        return value
+      default:
+        throw new SyntaxError(`unexpected key "${key}"`)
     }
+  }
 
-    private static reviver(key: string, value: {}): {} {
-        switch (key) {
-            case "":
-                return value
-            case "start":
-                if (typeof value !== "string")
-                    throw new SyntaxError('"start" must be a string')
-                const start = Date.parse(value)
-                if (isNaN(start))
-                    throw new SyntaxError(`"${value}" could not be parsed as date`)
-                return new Date(start)
-            case "count":
-                if (typeof value !== "number") {
-                    throw new SyntaxError('"count" must be a number')
-                }
-                return value
-            default:
-                throw new SyntaxError(`unexpected key "${key}"`)
-        }
-    }
-
-    constructor(public start: Date, public count: number) { }
+  constructor(public start: Date, public count: number) { }
 }
--- src/api/url.ts
+++ src/api/url.ts
@@ -1,6 +1,18 @@
-export const baseURL = "https://eee.lsgalfer.it/api"
+export const baseURL = process.env.NODE_ENV === "production" ?
+  "https://eee.lsgalfer.it" :
+  "http://localhost:9000"
 
-export const eventURL = `${baseURL}/event`
+export const apiURL = `${baseURL}/api`
+
+export const wsBaseURL = process.env.NODE_ENV === "production" ?
+  "wss://eee.lsgalfer.it/api" :
+  "ws://localhost:9000/api"
+
+export const loginURL = `${baseURL}/login`
+
+export const eventURL = `${apiURL}/event`
 
 export const eventsCountURL = `${eventURL}/count`
 export const eventBinsURL = `${eventURL}/bins`
+
+export const eventStreamWS = `${wsBaseURL}/event/stream`
