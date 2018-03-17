daa2cd58bc0fcdd30843a712959d421ccee9e015
Daniele Monteleone <daniele@monteleone.ml>
2018-03-17 19:35:00 +0100

    Remove blueprintjs from Charts

--- src/ui/BinsChart.tsx
+++ src/ui/BinsChart.tsx
@@ -1,55 +1,55 @@
 import * as React from "react"
-import { BarChart, ResponsiveContainer, CartesianGrid, Bar, Tooltip, XAxis } from "recharts"
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis } from "recharts"
+
 import { getEventBins } from "../api/event"
-import { Slider, Spinner } from "@blueprintjs/core"
 
 function countMap(iter: number[]): Map<number, number> {
-    const map = new Map<number, number>()
+  const map = new Map<number, number>()
 
-    for (let x of iter) {
-        map.set(x, (map.get(x) || 0) + 1)
-    }
+  for (let x of iter) {
+    map.set(x, (map.get(x) || 0) + 1)
+  }
 
-    return new Map(Array.from(map).sort((a, b) => a[0] - b[0]))
+  return new Map(Array.from(map).sort((a, b) => a[0] - b[0]))
 }
 
 interface State {
-    data: Map<number, number>
-    sliderValue: number
-    loading: boolean
-    error: Error
+  data: Map<number, number>
+  sliderValue: number
+  loading: boolean
+  error: Error
 }
 
 export class BinsChart extends React.Component<{}, State> {
-    getEvents() {
-        this.setState({ ...this.state, loading: true })
-        getEventBins(
-            `${this.state.sliderValue}s`,
-            new Date("2017/01/01"),
-            new Date()
-        )
-            .then(bins => {
-                this.setState({ ...this.state, data: countMap(bins.map(x => x.count)), loading: false })
-            })
-            .catch(error => this.setState({ ...this.state, error }))
-    }
+  getEvents() {
+    this.setState({ ...this.state, loading: true })
+    getEventBins(
+      `${this.state.sliderValue}s`,
+      new Date("2017/01/01"),
+      new Date()
+    )
+      .then(bins => {
+        this.setState({ ...this.state, data: countMap(bins.map(x => x.count)), loading: false })
+      })
+      .catch(error => this.setState({ ...this.state, error }))
+  }
 
-    componentWillMount() { this.setState({ sliderValue: 3, data: new Map(), loading: true }) }
+  componentWillMount() { this.setState({ sliderValue: 3, data: new Map(), loading: true }) }
 
-    componentDidMount() { this.getEvents() }
+  componentDidMount() { this.getEvents() }
 
-    render() {
-        return (
-            <div>
-                <ResponsiveContainer width="100%" height={250} >
-                    <BarChart data={Array.from(this.state.data)}>
-                        <CartesianGrid />
-                        <Tooltip />
-                        <XAxis dataKey="0" />
-                        <Bar dataKey="1" fill="#37474f" />
-                    </BarChart>
-                </ResponsiveContainer>
-                {this.state.loading &&
+  render() {
+    return (
+      <div>
+        <ResponsiveContainer width="100%" height={250} >
+          <BarChart data={Array.from(this.state.data)}>
+            <CartesianGrid />
+            <Tooltip />
+            <XAxis dataKey="0" />
+            <Bar dataKey="1" fill="#37474f" />
+          </BarChart>
+        </ResponsiveContainer>
+        {/* {this.state.loading &&
                     <Spinner />
                 }
                 <Slider
@@ -61,8 +61,8 @@ export class BinsChart extends React.Component<{}, State> {
                     onRelease={_ => this.getEvents()}
                     value={this.state.sliderValue}
                 // vertical={vertical}
-                />
-            </div>
-        )
-    }
+                /> */}
+      </div>
+    )
+  }
 }
--- src/ui/RealTimeChart.tsx
+++ src/ui/RealTimeChart.tsx
@@ -2,63 +2,63 @@ import * as React from "react"
 import { Bin } from "../api/type/event"
 import { getEventBins } from "../api/event"
 import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, ResponsiveContainer } from "recharts"
-import { Slider, Checkbox } from "@blueprintjs/core"
+// import { Slider, Checkbox } from "@blueprintjs/core"
 
 interface Props {
-    sample: string
-    // start: Date
-    stop: Date
+  sample: string
+  // start: Date
+  stop: Date
 }
 
 interface State {
-    bins: Bin[]
-    error: Error
-    sliderValue: number
-    active: boolean
+  bins: Bin[]
+  error: Error
+  sliderValue: number
+  active: boolean
 }
 
 export class RealTimeChart extends React.Component<Props, State> {
-    getEvents() {
-        getEventBins(
-            `${this.state.sliderValue}s`,
-            new Date(new Date().getTime() - 240000),
-            this.props.stop
-        )
-            .then(bins => {
-                this.setState({ ...this.state, bins: bins.slice(1) })
-            })
-            .catch(error => this.setState({ ...this.state, error }))
-    }
+  getEvents() {
+    getEventBins(
+      `${this.state.sliderValue}s`,
+      new Date(new Date().getTime() - 240000),
+      this.props.stop
+    )
+      .then(bins => {
+        this.setState({ ...this.state, bins: bins.slice(1) })
+      })
+      .catch(error => this.setState({ ...this.state, error }))
+  }
 
-    componentWillMount() { this.setState({ bins: [], sliderValue: 10, active: true }) }
+  componentWillMount() { this.setState({ bins: [], sliderValue: 10, active: true }) }
 
-    componentDidMount() {
-        this.getEvents()
-    }
+  componentDidMount() {
+    this.getEvents()
+  }
 
-    componentDidUpdate() {
-        if (this.state.active)
-            setTimeout(() => this.getEvents(), 300)
-    }
+  componentDidUpdate() {
+    if (this.state.active)
+      setTimeout(() => this.getEvents(), 300)
+  }
 
-    render() {
-        return (
-            <div style={{ marginTop: "1.5em" }}>
-                <ResponsiveContainer width="100%" height={200}>
-                    <BarChart
-                        style={{ marginLeft: "-1.3em" }}
-                        // margin={{ top: 20, right: 30, left: 0, bottom: 0 }}
-                        data={this.state.bins.map(i => { return { start: i.start.toLocaleString(), count: i.count } })}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="start" />
-                        <YAxis dataKey="count" />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey="count" fill="#607d8b" />
-                    </BarChart>
-                </ResponsiveContainer>
-                <Slider
+  render() {
+    return (
+      <div style={{ marginTop: "1.5em" }}>
+        <ResponsiveContainer width="100%" height={200}>
+          <BarChart
+            style={{ marginLeft: "-1.3em" }}
+            // margin={{ top: 20, right: 30, left: 0, bottom: 0 }}
+            data={this.state.bins.map(i => { return { start: i.start.toLocaleString(), count: i.count } })}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="start" />
+            <YAxis dataKey="count" />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#607d8b" />
+          </BarChart>
+        </ResponsiveContainer>
+        {/* <Slider
                     min={5}
                     max={20}
                     stepSize={1}
@@ -70,8 +70,8 @@ export class RealTimeChart extends React.Component<Props, State> {
                 <Checkbox
                     checked={this.state.active}
                     onChange={x => this.setState({ ...this.state, active: x.currentTarget.checked })}
-                />
-            </div>
-        )
-    }
+                /> */}
+      </div>
+    )
+  }
 }
