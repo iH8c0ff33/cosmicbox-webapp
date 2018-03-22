38e2613bc3ad226ba8560d91f849148e8c40a3b1
Daniele Monteleone <daniele@monteleone.ml>
2018-03-23 00:00:59 +0100

    Add WebSocket urls

--- src/api/url.ts
+++ src/api/url.ts
@@ -4,15 +4,18 @@ export const baseURL = process.env.NODE_ENV === "production" ?
 
 export const apiURL = `${baseURL}/api`
 
-export const wsBaseURL = process.env.NODE_ENV === "production" ?
-  "wss://eee.lsgalfer.it/api" :
-  "ws://localhost:9000/api"
-
 export const loginURL = `${baseURL}/login`
 
 export const eventURL = `${apiURL}/event`
 
 export const eventsCountURL = `${eventURL}/count`
 export const eventBinsURL = `${eventURL}/bins`
+export const eventRangeURL = `${eventURL}/range`
 
-export const eventStreamWS = `${wsBaseURL}/event/stream`
+export const wsBaseURL = process.env.NODE_ENV === "production" ?
+  "wss://eee.lsgalfer.it" :
+  "ws://localhost:9000"
+
+export const wsApiURL = `${wsBaseURL}/api`
+
+export const eventStreamWS = `${wsApiURL}/event/stream`
