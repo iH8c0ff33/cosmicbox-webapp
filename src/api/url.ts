f8bbc72e2be0c61816718ca1b76cc6a1a0ec032b
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2018-10-13 15:26:14 +0200

    Fix callback URLs again

--- src/api/url.ts
+++ src/api/url.ts
@@ -2,11 +2,10 @@ export const baseURL = process.env.NODE_ENV === "production" ?
   "https://eee.lsgalfer.it" :
   "http://localhost:9000"
 
-export const baseUIURL = process.env.NODE_ENV === "production" ?
-  `${location.origin}/dashboard` :
-  location.origin
+export const dashboardURL = process.env.NODE_ENV === "production" ?
+  "/dashboard" : ""
 
-export const callbackURL = `${baseUIURL}/callback`
+export const callbackURL = `${location.origin + dashboardURL}/callback`
 
 export const apiURL = `${baseURL}/api`
 
@@ -17,7 +16,7 @@ export const eventURL = `${apiURL}/event`
 export const eventsCountURL = `${eventURL}/count`
 export const eventBinsURL = `${eventURL}/bins`
 export const eventRangeURL = `${eventURL}/range`
-export const pressureAvgURL =  `${eventURL}/press`
+export const pressureAvgURL = `${eventURL}/press`
 
 export const wsBaseURL = process.env.NODE_ENV === "production" ?
   "wss://eee.lsgalfer.it" :
--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import AuthCallback from "./api/auth/Callback"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
+import { dashboardURL } from "./api/url"
 
 ReactDOM.render(
   <Router>
     <>
       <Route path="/" component={App} />
-      <Route path="/callback" component={AuthCallback} />
+      <Route path={dashboardURL + "/callback"} component={AuthCallback} />
     </>
   </Router>,
   document.getElementById("root")!
